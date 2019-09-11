d20caa7742582d737726398623cc914fca76d4d8
Claire Filipek <31299149+cfilipek@users.noreply.github.com>
2019-09-11 06:03:20 -0400

    Added demo video link and new technologies used

--- src/components/Grace.js
+++ src/components/Grace.js
@@ -18,12 +18,12 @@ const Grace = () => {
       </Row>
       <Row className="home-top-image-row">
         <h4>The goal of the app is to help an elderly alien named grace, who drops her bag and loses her belongings, collect all her belongings in a virtual reality space scene. Wherein the user clicks on the screen to collect the items in a given timeframe. I contributed greatly to the Front-end; by working on the styling, creating 3D objects and images for the app.<br/><br/> In addition, I worked on functionalities such as the ability to navigate from static pages to the VR scene, as well as applying a static footer in the VR scene, and creating loading states to wait for large image files to appear before loading pages in fully.</h4>
-        <h4 className="project-tech"><span className="bold-text">Technology used:</span> React-Native, Viro React, Redux</h4>
+        <h4 className="project-tech"><span className="bold-text">Technology used:</span> React-Native, Viro React, Redux, Firebase, Firestore</h4>
       </Row>
       <div className="project-tech bold-center-links">
         <Row className="project-link-space-top">
           <Col sm={6}>
-             <a href="" target="_blank" rel="noopener noreferrer">Video Link</a>
+             <a href="https://www.youtube.com/watch?v=Tg7BLim7ues&list=PLx0iOsdUOUmnZXS2H9EDbhYMlmh3onWHs&index=6&t=0s" target="_blank" rel="noopener noreferrer">Video Link</a>
           </Col>
            <Col>
             <a href="https://github.com/graceinspace/graceInSpace" target="_blank" rel="noopener noreferrer">Github Link</a>
@@ -32,12 +32,12 @@ const Grace = () => {
       </div>
       <Row className="home-top-image-row">
         <Col className="project-center-image">
-          <img src={graceGif} alt="TCY shop page"/>
+          <img src={graceGif} alt="Grace gif"/>
         </Col>
       </Row>
       <Row className="home-top-image-row">
         <Col className="project-center-image">
-          <img className="project-image-resize" src={wireframe} alt="TCY shop page"/>
+          <img className="project-image-resize" src={wireframe} alt="wireframes"/>
         </Col>
       </Row>
       <div className="spacer"></div>
